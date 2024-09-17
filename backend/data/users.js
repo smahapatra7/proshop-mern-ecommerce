0d888b8adbbecdd914330a6ac4beb26ea2f8f2b4
Shibani Mahapatra <shibanimahapatra.7@gmail.com>
2024-09-17 21:13:37 +0530

    postman

---
+++ backend/data/users.js
@@ -0,0 +1,28 @@
+import bcrypyt from 'bcryptjs';
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@email.com',
+        password: bcrypyt.hashSync('12345', 10),
+        isAdmin: true,
+
+    },
+    {
+        name: 'John Doe',
+        email: 'john@email.com',
+        password: bcrypyt.hashSync('12345', 10),
+        isAdmin: false,
+
+    },
+    {
+        name: 'Jane Doe',
+        email: 'jane@email.com',
+        password: bcrypyt.hashSync('12345', 10),
+        isAdmin: false,
+
+    }
+
+];
+
+export default users;
