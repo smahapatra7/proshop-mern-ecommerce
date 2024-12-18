05652ae5065b13ee77b6e835779328155e041966
Shibani Mahapatra <shibanimahapatra.7@gmail.com>
2024-12-18 20:14:16 +0530

    Qty & Add to cart handler

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -14,7 +14,7 @@ const cartSlice = createSlice({
         addToCart: (state, action) => {
             const item = action.payload;
 
-            const existItem = state.cartItem.find((x) => x._id === item._id);
+            const existItem = state.cartItems.find((x) => x._id === item._id);
 
             if(existItem){
                 state.cartItems = state.cartItems.map((x) => x._id === existItem._id ? item : x)
