1effae6df237874ebd8b5d1135dc38faa1b30ff7
Shibani Mahapatra <shibanimahapatra.7@gmail.com>
2024-12-26 21:44:51 +0530

    user routes and controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,7 +4,6 @@ import Product from '../models/productModel.js';
 // @desc   Fetch all products
 // @route  GET /api/products
 // @access Public
-
 const getProducts = asyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.json(products);
