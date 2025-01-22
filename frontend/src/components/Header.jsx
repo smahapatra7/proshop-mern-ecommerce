3753d818896ee893d9926a0d4cb7b93d9e5dcc0d
Shibani Mahapatra <shibanimahapatra.7@gmail.com>
2025-01-22 15:37:07 +0530

    user logout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,30 @@
+import { useNavigate } from 'react-router-dom';
 import { Badge, Navbar, Nav, Container, NavLink, NavDropdown } from 'react-bootstrap';
 import {FaShoppingCart, FaUser} from 'react-icons/fa';
-import { useSelector } from 'react-redux';
-import logo from '../assets/logo.png'
-import {LinkContainer} from 'react-router-bootstrap'
-
+import { useSelector, useDispatch } from 'react-redux';
+import logo from '../assets/logo.png';
+import {LinkContainer} from 'react-router-bootstrap';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
 import React from 'react';
 
 const Header = () => {
     const { cartItems } = useSelector((state) => state.cart);
     const { userInfo } = useSelector((state) => state.auth);
 
-    const logoutHandler = () => {
-        console.log('logout');
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [logoutApiCall] = useLogoutMutation();
+
+    const logoutHandler = async () => {
+        try {
+            await logoutApiCall().unwrap();
+            dispatch(logout());
+            navigate('/login');
+        } catch (err) {
+            console.log(err);
+        }
     };
     
     return (
