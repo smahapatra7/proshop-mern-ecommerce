fc22f32168e80a9fff192c880535456e749998c3
Shibani Mahapatra <shibanimahapatra.7@gmail.com>
2025-02-04 18:29:06 +0530

    Shipping screen

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,7 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { updateCart } from "../utils/cartUtils";
 
-const initialState = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : {cartItems: []}; 
+const initialState = localStorage.getItem("cart") 
+    ? JSON.parse(localStorage.getItem("cart")) : 
+    {cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' }; 
 
 const cartSlice = createSlice({
     name: "cart",
@@ -25,9 +27,13 @@ const cartSlice = createSlice({
 
             return updateCart(state);
         },
+        saveShippingAddress: (state, action) => {
+            state.shippingAddress = action.payload;
+            return updateCart(state);
+        }
     },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress } = cartSlice.actions;
 
 export default cartSlice.reducer;
