a7bf24e0fc9f2a8b3a25a600a6e28802c5f970fb
Shibani Mahapatra <shibanimahapatra.7@gmail.com>
2025-06-24 23:04:03 +0530

    order page

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -15,7 +15,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
     totalPrice,
   } = req.body;
 
-  if (orderItems && orderItems.length == 0){
+  if (orderItems && orderItems.length === 0){
   res.status(400);
   throw new Error('No order items');
   } else {
@@ -53,7 +53,7 @@ const getMyOrders = asyncHandler(async (req, res) => {
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
-  const orders = await Order.findById(req.params.id).populate('user', 'name email');
+  const order = await Order.findById(req.params.id).populate('user', 'name email');
 
   if (order) {
     res.status(200).json(order);
