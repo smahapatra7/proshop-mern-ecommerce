1443775ec2c9ab321ae0f342386c4c3e0d5a61b7
Shibani Mahapatra <shibanimahapatra.7@gmail.com>
2025-06-25 23:21:58 +0530

    PayPal Buttons

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -69,14 +69,14 @@ const getOrderById = asyncHandler(async (req, res) => {
 const updateOrderToPaid = asyncHandler(async (req, res) => {
   const order = await Order.findById(req.params.id);
 
-  if (order){
+  if (order) {
     order.isPaid = true;
     order.paidAt = Date.now();
     order.paymentResult = {
       id: req.body.id,
       status: req.body.status,
       update_time: req.body.update_time,
-      email_address: req.bosy.payer.email_address,
+      email_address: req.body.payer.email_address,
     };
 
     const updatedOrder = await order.save();
--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -43,6 +43,44 @@ const OrderScreen = () => {
         }
     }, [order, paypal, paypalDispatch, loadingPayPal, errorPayPal]);
 
+    function onApprove(data, actions) { 
+        return actions.order.capture().then(async function (details) { 
+            try {
+                await payOrder({ orderId, details });
+                refetch();
+                toast.success('Payment successful');
+            } catch (error) {
+                toast.error(error?.data?.message || error.message);
+            }
+        });
+        
+    }
+
+    async function onApproveTest() { 
+        await payOrder({ orderId, details: { payer: {} } });
+        refetch();
+        toast.success('Payment successful');
+    }
+
+    function onError(err) { 
+        toast.error(err.message);
+    }
+
+    function createOrder(data, actions) { 
+        return actions.order.create({
+            purchase_units: [
+                {
+                    amount: {
+                        value: order.totalPrice,
+                    },
+                },
+            ],
+        }).then((orderId) => { 
+            return orderId;
+        });
+    }
+
+
     return isLoading ? <Loader/> : error ? < Message variant="danger">{error?.data?.message || error?.error || 'Something went wrong'}</Message>
  : (
         <>
@@ -133,7 +171,21 @@ const OrderScreen = () => {
                                 </Row>
                             </ListGroup.Item>
                             
-                            {/* PAY ORDER PLACEHOLDER */}
+                            
+                            { !order.isPaid && (
+                                <ListGroup.Item>
+                                    {loadingPay && <Loader/>}
+
+                                    {isPending ? <Loader /> : (
+                                        <div>
+                                            {/* <Button onClick={ onApproveTest } style = {{marginBottom: '10px'}}>Test Pay Order</Button> */}
+                                            <div>
+                                                <PayPalButtons createOrder={ createOrder } onApprove={ onApprove } onError={ onError }></PayPalButtons>
+                                            </div>
+                                        </div>
+                                    )}
+                                </ListGroup.Item>
+                            )}
                             {/* MARK AS DELIVERED PLACEHOLDER */}
                             
                             </ListGroup> 
